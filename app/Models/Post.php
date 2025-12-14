dd581c08893557c4eddcf730b83dc9d420a0c878
DevWithShakil <m.shakil720@gmail.com>
2025-12-15 00:06:58 +0600

    category, post and user model implemented

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -2,9 +2,33 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Post extends Model
 {
-    //
+    use HasFactory;
+
+    protected $fillable = [
+        'category_id',
+        'user_id',
+        'title',
+        'slug',
+        'content',
+        'image',
+        'status',
+        'published_at'
+    ];
+
+    // Relationship: Post belongs to Category
+    public function category()
+    {
+        return $this->belongsTo(Category::class);
+    }
+
+    // Relationship: Post belongs to User (Author)
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
 }
